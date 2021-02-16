5b99362664a7a21037eaf4c65ff08cac29268137
jzmr_0409@hotmail.com <juanrivera09>
2021-02-16 18:38:15 -0500

    se agrego proveedor

---
+++ src/main/java/com/trabajo/pedidos/model/Proveedor.java
@@ -0,0 +1,35 @@
+package com.trabajo.pedidos.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Proveedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 30)
+    private String nombreEmpresa;
+
+    @Column(length = 30)
+    private String nombreContacto;
+
+    @Column(length = 70)
+    private String direccion;
+
+    @Column(length = 20)
+    private String ciudad;
+
+    @Column(length = 9)
+    private String telefono;
+
+
+}
